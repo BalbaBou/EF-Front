d3fb4dd91ca6025e379ca422f1af77075afd5b86
unknown <kapshuk99@inbox.ru>
2024-03-30 09:17:17 +0200

    first

---
+++ src/screens/Direction/Direction.jsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { Link, useLocation, useParams } from "react-router-dom"
+import Header from "../../components/Layout/Header/Header"
+import './Direction.css'
+import Footer from "../../components/Layout/Footer/Footer"
+import Loader from "../../components/UI/Loader/Loader"
+const formEducation = ["Очная", "Заочная", "Очно-заочная"]
+function Direction() {
+    const { directionId: profileId } = useParams()
+    const { state } = useLocation()
+
+    const [profile, setProfile] = useState({})
+    const [yearRecruitments, setYearRecruitments] = useState({})
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    const fetchDirectionById = async () => {
+        console.log("id: " + profileId)
+        console.log("state-id: " + state.directionsId)
+        await axios({ method: "get", url: "https://localhost:80/api/Profile/GetById?id=" + profileId })
+            .then((e) => {
+
+                console.log(e.data);
+                setProfile(e.data)
+                let currentYear = (new Date()).getFullYear()
+                let year = e.data.yearRecruitments[0].year
+                console.log(year)
+                e.data.yearRecruitments && e.data.yearRecruitments.map((i) => {
+                    if ((new Date(i.year)).getFullYear() == currentYear)
+                        setYearRecruitments(i)
+                })
+                setIsLoading(false)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        // fetch('/data.json')
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         const id = 1 //state.directionsId
+        //         const directions = data.data.find(item => item.id === Number(id))
+        //         setDirection(directions?.directions.find(d => d.id === Number(12)))
+        //         setIsLoading(false)
+
+        //     })
+    }
+
+    useEffect(() => {
+        fetchDirectionById()
+    }, [profileId])
+
+    console.log(state)
+
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <div className="path">
+                    <div className="path_item">
+                        <Link to={'/'} className="path_link">Главная</Link>
+                        <div className="path_arrow">
+                            <i className='bx bx-chevron-right'></i>
+                        </div>
+                    </div>
+                    <div className="path_item">
+                        <Link to={`/directions/${state.directionsId}`} className="path_link">Программы бакалавриата и магистратуры</Link>
+                        <div className="path_arrow">
+                            <i className='bx bx-chevron-right'></i>
+                        </div>
+                    </div>
+                    <div className="path_item">
+                        <p className="path_link">{profile?.name}</p>
+                    </div>
+                </div>
+                {isLoading ? <Loader /> : (
+                    <>
+                        <h1 className="title">{profile?.name}</h1>
+                        <p className="direction-about">{profile?.description}</p>
+                        <h2 className="subtitle">Подробнее о специальности</h2>
+                        <div className="direction-info">
+                            <div>
+                                <h3 className="info_title">Период обучения</h3>
+                                <div className="info_scores">                                    
+                                        <div className="subject">
+                                            <p className="subject_score">{profile.specificationsList[0].periodEducation}</p>
+                                            <p className="subject_name">{profile.specificationsList[0].periodEducation == 4? "года": "лет"}</p>
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className="info_title">Количество мест</h3>
+                                <div className="info_scores">
+
+                                    <div className="subject" >
+                                        <p className="subject_score">{yearRecruitments.numberSeatsList[0].budgetPlaces}</p>
+                                        <p className="subject_name">{"Бюджетных"}</p>
+                                    </div>
+                                    <div className="subject" >
+                                        <p className="subject_score">{yearRecruitments.numberSeatsList[0].paidPlaces}</p>
+                                        <p className="subject_name">{"Платных"}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className="info_title">Форма обучения</h3>
+                                <div className="info_scores">
+                                    
+                                        <div  className="subject">
+                                            <p className="subject_score">{formEducation[profile.specificationsList[0].formEducation]}</p>
+                                            {/* <p className="subject_name">{score.subject}</p> */}
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className="info_title">Стоимость обучения</h3>
+                                <div className="info_scores">
+                                    <div className="subject">
+                                        <p className="subject_score">{profile.specificationsList[0].price} руб.</p>
+                                        <p className="subject_name" style={{ visibility: "hidden" }}>1</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h2 className="subtitle">Дисциплины</h2>
+                        <div className="courses">
+                            {yearRecruitments.yearRecruitment_Disciplines && yearRecruitments.yearRecruitment_Disciplines.map((year) => (
+                                <Link
+                                    state={{ directionId: profileId, directionsId: state.directionsId }}
+                                    to={`/courses/course/${year.discipline.id}`}
+                                    key={year.discipline.id}
+                                    className="courses_item">
+                                    <p className="courses_item-name">{year.discipline.name}</p>
+                                </Link>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div >
+            {!isLoading && <Footer />
+            }
+        </>
+    )
+}
+
+export default Direction
---
+++ src/screens/Directions/Directions.jsx
@@ -0,0 +1,155 @@
+import { Link, useParams } from "react-router-dom"
+import Header from "../../components/Layout/Header/Header"
+
+import axios from "axios";
+import './Directions.css'
+import { useCallback, useEffect, useState } from "react"
+import Footer from "../../components/Layout/Footer/Footer"
+import Loader from "../../components/UI/Loader/Loader"
+
+function Directions() {
+    const { directionsId } = useParams()
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    const [isChecked, setIsChecked] = useState({ bachelor: false, master: false, speciality: false })
+
+    const changeDirectionsHandler = useCallback(() => {
+        if (!isChecked.bachelor && !isChecked.master && !isChecked.speciality) {
+            setFilteredProfiles(profiles)
+        } else if (isChecked.bachelor && isChecked.master && isChecked.speciality) {
+            setFilteredProfiles(profiles)
+        } else if (isChecked.bachelor) { //бакалавриат
+            setFilteredProfiles(profiles.filter(p => p.specificationsList[0].levelEducation === 0))
+        } else if (isChecked.master) {  //магистратура
+            setFilteredProfiles(profiles.filter(p => p.specificationsList[0].levelEducation === 1))
+        } else if (isChecked.speciality) {  //специалитет
+            setFilteredProfiles(profiles.filter(p => p.specificationsList[0].levelEducation === 2))
+            console.log(filteredProfiles)
+        }
+
+    }, [isChecked])
+
+    const [filteredProfiles, setFilteredProfiles] = useState([])
+    const [profiles, setProfiles] = useState([])
+    const [nameGeneralDirection, setNameGeneralDirection] = useState('')
+    const [classPlaceholder, setClassPlaceholder] = useState(['placeholder'])
+
+    const fetchDirectionsById = useCallback(async () => {
+        console.log(directionsId)
+        await axios({ method: "get", url: "https://localhost:80/api/Direction/GetByGeneralDirection?id=" + directionsId })
+            .then((e) => {
+                console.log(e.data);
+                if (e.data.length != 0) {
+                    let array_profiles = []
+                    e.data.forEach((k) => k.profiles.forEach((e) => { e.nameDirection = k.name; array_profiles.push(e); }))
+                    console.log(array_profiles);
+                    setProfiles(array_profiles)
+                    setFilteredProfiles(array_profiles)
+                    setNameGeneralDirection(e.data[0].generalDirection.name)                    
+                }
+                setIsLoading(false)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        // fetch('/data.json')
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         const directions = data.data.find(item => item.id === Number(directionsId))
+        //         setDirections(directions.directions)
+        //         setFilteredDirections(directions.directions)
+        //         setNameDirection(directions.name)
+
+        //         setIsLoading(false)
+        //     })
+    }, [directionsId])
+
+    useEffect(() => {
+        fetchDirectionsById()
+    }, [directionsId])
+
+    useEffect(() => {
+        changeDirectionsHandler()
+    }, [isChecked])
+
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <div className="path">
+                    <div className="path_item">
+                        <Link to={'/'} className="path_link">Главная</Link>
+                        <div className="path_arrow">
+                            <i className='bx bx-chevron-right'></i>
+                        </div>
+                    </div>
+                    <div className="path_item">
+                        <p className="path_link">Программы бакалавриата и магистратуры</p>
+                    </div>
+                </div>
+                <h1 className="title">Программы бакалавриата и магистратуры</h1>
+                <div className="search-direction">
+                    <input type="text" className="search-direction_input"
+                        onFocus={() => (setClassPlaceholder(prev => ([...prev, 'show'])))}
+                        onBlur={() => (setClassPlaceholder(['placeholder']))}
+                        onInput={(e) => {
+                            if (e.target.value.length === 0) {
+                                setClassPlaceholder(prev => ([...prev, 'show']))
+                            } else {
+                                setClassPlaceholder(['placeholder'])
+                            }
+                        }} />
+                    <div className={classPlaceholder.join(' ')}>Поиск по специальностям</div>
+                    <div className="search-direction_icon">
+                        <i className='bx bx-search' ></i>
+                    </div>
+                </div>
+                {isLoading ? <Loader /> : (
+                    <div className="list-direction">
+                        <div className="list-direction_filter">
+                            <div>
+                                <h3>Выберите форму обучения</h3>
+                                <div className="filter_item">
+                                    <input type="checkbox" className="checkbox" id="bachelor"
+                                        checked={isChecked.bachelor} onChange={(e) => setIsChecked(prev => ({ ...prev, bachelor: e.target.checked }))} />
+                                    <label htmlFor="bachelor">Бакалавриат</label>
+                                </div>
+                                <div className="filter_item">
+                                    <input type="checkbox" className="checkbox" id="master"
+                                        checked={isChecked.master} onChange={(e) => setIsChecked(prev => ({ ...prev, master: e.target.checked }))} />
+                                    <label htmlFor="master">Магистратура</label>
+                                </div>
+                                <div className="filter_item">
+                                    <input type="checkbox" className="checkbox" id="speciality"
+                                        checked={isChecked.speciality} onChange={(e) => setIsChecked(prev => ({ ...prev, speciality: e.target.checked }))} />
+                                    <label htmlFor="speciality">Специалитет</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="list-direction_items">
+                            {filteredProfiles && filteredProfiles.map(p => (
+                                <Link
+                                    to={`/directions/direction/${p.id}`}  //p.id directionsId
+                                    state={{ directionsId }}
+                                    key={p.id}
+                                    className="list-direction_items-card">
+                                    <div className="card-title">
+                                        <h3>{p.nameDirection}</h3>
+                                        <p>{p.specificationsList[0].levelEducation == 0 ? 'бакалавр' : p.specificationsList[0].levelEducation == 1 ? 'магистр' : 'специалист'}
+                                            <i className='bx bx-right-arrow-alt' ></i></p>
+                                    </div>
+                                    <div className="card-desc">{p.name}</div>
+                                    <span className="card-mark">{nameGeneralDirection}</span>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+            {!isLoading && <Footer />}
+        </>
+    )
+}
+
+export default Directions
